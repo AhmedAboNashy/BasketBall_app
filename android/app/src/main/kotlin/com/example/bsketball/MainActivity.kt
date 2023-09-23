66223168dcff0881ce15a90649b429f3d61e1b26
AhmedAbonashy <ahmedabonashy1000@gmail.com>
2023-09-23 19:12:13 +0300

    create_homeScreen but empty

---
+++ android/app/src/main/kotlin/com/example/bsketball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bsketball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/HomeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+static const String route_Name='home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
